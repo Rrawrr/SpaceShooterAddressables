9582f10d459da966bec00ee4070e8041e915a3c5
Rrawrr <l0sty@yandex.ru>
2021-04-01 23:21:35 +0300

    settings added

--- Assets/SpaceShooter/Scripts/SceneLoader.cs
+++ Assets/SpaceShooter/Scripts/SceneLoader.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.Serialization;
 
 public class SceneLoader : MonoBehaviour
 {
     public string nextSceneAddress;
+    AsyncOperationHandle preloadOp;
     
     // Start is called before the first frame update
     void Start()
     {
-        Addressables.LoadSceneAsync(nextSceneAddress);
+        Addressables.DownloadDependenciesAsync("preload").Completed += OnContentDownloaded;
     }
+
+    void OnContentDownloaded(AsyncOperationHandle op)
+    {
+        Debug.Log("CONTENT DOWNLOADED");
+        Addressables.LoadSceneAsync(nextSceneAddress).Completed += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(AsyncOperationHandle<SceneInstance> op)
+    {
+        Debug.Log("SCENE LOADED");
+    }
+
+
 }
